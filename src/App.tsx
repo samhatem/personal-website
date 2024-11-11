23e18f651a1e163eb75ed950659de70e1b1b64ff
samhatem <sam.hatem17@gmail.com>
2024-11-10 19:55:06 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const bio = `
 
 Previous experience includes building early infrastucutre at [Polymarket](https://polymarket.com) and raising $12m for a startup (and then later giving it back).
 
-Angel investor in [Antimetal](https://www.antimetal.com/) (seed), [Silna](https://www.silnahealth.com/) (seed), [Prophetic](https://propheticai.co/) (pre-seed), [Vultron](https://www.vultron.ai/) (pre-seed), [Modern Intelligence](https://www.modernintelligence.ai/) (seed), and [Praxis](https://www.praxisnation.com/) (prior to any VC funding).
+Angel investor in [Antimetal](https://www.antimetal.com/) (seed), [Silna](https://www.silnahealth.com/) (seed), [Prophetic](https://propheticai.co/) (pre-seed), [Vultron](https://www.vultron.ai/) (pre-seed), [Modern Intelligence](https://www.modernintelligence.ai/) (seed), [Tenkara](https://www.trytenkara.com/) (pre-seed) and [Praxis](https://www.praxisnation.com/) (prior to any VC funding).
 `;
 
 const App: React.FC = () => {
