05222973b2b4310f81d90bf7104bbb5b7f8f92d4
samhatem <sam.hatem17@gmail.com>
2025-11-26 14:56:04 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const bio = `
 
 Building [ChainPro](https://chainpro.xyz). Previously built early infrastucutre at [Polymarket](https://polymarket.com).
 
-Angel investor in [Antimetal](https://www.antimetal.com/) (seed), [Silna](https://www.silnahealth.com/) (seed), [Prophetic](https://propheticai.co/) (pre-seed), [Vultron](https://www.vultron.ai/) (pre-seed), [Modern Intelligence](https://www.modernintelligence.ai/) (seed), [Tenkara](https://www.trytenkara.com/) (pre-seed) and [Praxis](https://www.praxisnation.com/) (prior to any VC funding).
+Angel investor in [Antimetal](https://www.antimetal.com/) (seed), [Silna](https://www.silnahealth.com/) (seed), [Prophetic](https://propheticai.co/) (first investor), [Vultron](https://www.vultron.ai/) (pre-seed), [Modern Intelligence](https://www.modernintelligence.ai/) (seed), [Tenkara](https://www.trytenkara.com/) (pre-seed) and [Praxis](https://www.praxisnation.com/) (prior to any VC funding).
 `;
 
 const App: React.FC = () => {
